ab3358e9541f529c39fd7cc07125cf1ffe952fda
Ethyde <ethyde@hotmail.fr>
2018-05-06 22:56:03 +0200

    Better

--- runremote.sh
+++ runremote.sh
@@ -4,9 +4,9 @@
 # from : http://backreference.org/2011/08/10/running-local-script-remotely-with-arguments/
 
 realscript=$1
-user=$2
-host=$3
-shift 3
+host1=shgate@107360_faucille10.nexen.net
+host2=studio_ssh@pmddeploy01 
+shift 1
 
 # escape the arguments
 declare -a args
@@ -20,16 +20,4 @@ done
 {
   printf '%s\n' "set -- ${args[*]}"
   cat "$realscript"
-} | ssh $user@$host "bash -s"
-
-
-# script_deploy.sh
-# 
-# ssh -t sshgate@107360_faucille10.nexen.net studio_ssh@pmddeploy01
-# cd deploy/
-# cap scds:[env] deploy -S application=[projet] -S branch=[version]
-# [domain=gitlab.com] cap scds:[env] deploy -S application=[projet] -S branch=[version]
-
-# runremote.sh script_deploy.sh [env] [projet] [version] [?:domain]
-
-# $4 cap scds:$1 deploy -S application=$2 -S branch=$3
+} | ssh -t $host1@$host2 "bash -s"
